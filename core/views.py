c24e3df2855d8126ad9671c46410906fe8184cd2
bakutake <loievskyi.slava@gmail.com>
2021-07-01 14:34:03 +0300

    Add Category endpoint

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
-from core.models import Currency
-from core.serializers import CurrencySerializer
+from core.models import Currency, Category
+from core.serializers import CurrencySerializer, CategorySerializer
 
 
 class CurrencyListAPIView(ListAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
+
+
+class CategoryModelViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
--- piggybank/urls.py
+++ piggybank/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 
 from core import views
 
 
+router = routers.SimpleRouter()
+
+router.register(r"categories", views.CategoryModelViewSet, basename="category")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("currencies/", views.CurrencyListAPIView.as_view(), name="currencies")
-]
+    path("currencies/", views.CurrencyListAPIView.as_view(), name="currencies"),
+] + router.urls
